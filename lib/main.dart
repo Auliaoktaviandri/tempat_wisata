4504445dbc5ecb96e59b3b2acf6be7489d550ff3
Auliaoktaviandri <116175601+Auliaoktaviandri@users.noreply.github.com>
2022-12-04 21:46:57 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Taman',
       theme: ThemeData(),
       home: const DetailSreen(),
